c18cdbfba8e83c12777d026b5c2894d39b1b2285
Shubham Basu <shubham.basu991@gmail.com>
2020-02-02 22:45:08 -0800

    feat:animation added

--- src/components/bars/bar/bar.jsx
+++ src/components/bars/bar/bar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import './style.css'
 const El = styled.div`background-color:#FEDC33;
 width:25px;
 height:${props=>props.height}px;`
+
+
 const bar = ({height = 0,active,sorted}) => 
 {
      const color = sorted?'bg-success':active?'bg-danger':'';
+     const test = active?' some':''
+     const some = 'col'+test;
       return( 
-      <div className="col">
+      <div className={some}>
             {height}<El height={height} className={color}></El>
       </div>
        );
--- src/components/bars/bars.jsx
+++ src/components/bars/bars.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; 
 import Bar from './bar/bar';
-import './style.css'
 
 const bars = ({ barArray = [],activeIndex,sortedIndex }) => {
   const barElements = barArray.map((num,index) => (<Bar height={num} key={index} active={activeIndex.includes(index)} sorted={index>=sortedIndex}/>));
